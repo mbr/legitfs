016d81d824a56739799564dc7f2652a7d88d5219
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-16 14:32:56 +0200

    Added translate path method.

--- legitfs/util.py
+++ legitfs/util.py
@@ -1,4 +1,4 @@
-from itertools import takewhile
+import os
 
 
 def split_using(predicate, iterable):
@@ -11,3 +11,20 @@ def split_using(predicate, iterable):
             break
 
     return top, [i for i in it]
+
+
+def translate_path(path):
+    """Translates a path into repository/repo_path components.
+
+    All paths will be considered relative, regardless of whether or not they
+    start with '/'.
+
+    :return: (leading_path, repo_path)
+    """
+    if path.startswith('/'):
+        path = path[1:]
+
+    return tuple(
+        os.path.join(*c) if c else '' for c in
+        split_using(lambda v: v.endswith('.git'), path.split(os.sep))
+    )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,18 @@
-from legitfs.util import split_using
+from legitfs.util import split_using, translate_path
+import pytest
 
 
 def test_split_using():
     assert (split_using(lambda v: v > 5, [5, 4, 2, 9, 8, 3])
             == ([5, 4, 2, 9], [8, 3]))
+
+
+@pytest.mark.parametrize('input, top, tail', [
+    ('foo.git', 'foo.git', ''),
+    ('test/.git', 'test/.git', ''),
+    ('hello/my/.git/refs/heads/master', 'hello/my/.git', 'refs/heads/master'),
+    ('sub.git/foo', 'sub.git', 'foo'),
+])
+def test_translate_path(input, top, tail):
+    assert translate_path(input) == (top, tail)
+    assert translate_path('/' + input) == (top, tail)
